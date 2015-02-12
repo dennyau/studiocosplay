697e2aa562593a4ab7dd33b7e1976ae63e77c4e5
Dennis Yau <denn.yau@gmail.com>
2015-02-12 18:15:07 -0500

    Updated 404 page

--- themes/studiocosplay/404.php
+++ themes/studiocosplay/404.php
@@ -1,11 +1,29 @@
 <?php
-/**
- * The template for displaying 404 pages (Not Found)
- *
- * @package WordPress
- * @subpackage StudioCosplay
- * @since Studio Cosplay 1.0
- */
 
-echo "Page not found";
+get_header();
+?>
 
+    <div class="title-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 cntr">
+                    <h1>Page not found =(</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="studioCosplay" style=" padding-left: 3%; padding-right: 3%; text-align: center; ">     
+        <div class="container">  
+            <div class="row"> 
+                <div class="inner">
+                    <p>Thanks for your interest, but your page is in another castle!</p>
+                    <p>Maybe checkout our <a href="/cosnews/">CosNews</a> for the latest that going on?</p>
+                    <p>Or just see everything about <a href="/">Studio Cosplay</a> =)</p>
+                </div> 
+            </div> 
+        </div> 
+    </div> 
+
+<?php
+get_footer();
